de2da9584dd0f73ee5c46c81b62b4b2d32039d90
alexrpedersen <alex.r.pedersen@outlook.com>
2020-11-03 14:38:40 -0800

    added interface auth props and fixed auth fc

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -5,8 +5,33 @@ import GoogleLogin from 'react-google-login';
 import { useDispatch, useSelector } from 'react-redux';
 import { LOGIN } from '../redux/actionTypes';
 
-const Auth: React.FC<> = props => {
-    
+interface AuthProps {
+    typeof: {LOGIN},
+    name: string,
+    picture: string
+}
+
+const Auth: React.FC<AuthProps> = () => {
+    const dispatch = useDispatch();
+
+    const auth = useSelector(state => state.auth);
 
 }
 
+const ProcessFacebookLogin: React.FC<AuthProps> = () => {
+    return;
+}
+
+const jumpTo = (step: number): void => {
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0)
+}
+
+if(Auth.isAuthenticated) {
+    return (
+        <div>
+            <img src={Auth.picture} alt={Auth.name} />
+            <h2>Welcome {Auth.name}</h2>
+        </div>
+    );
+}
